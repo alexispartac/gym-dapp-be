57e3e022563fff6d9a3a05e6547be80a6c333193
Partac <alexismateipartac@gmail.com>
2025-04-23 10:11:54 +0300

    fixed bugs

--- programs/gym-dapp-be/src/lib.rs
+++ programs/gym-dapp-be/src/lib.rs
@@ -1,5 +1,5 @@
+#![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
-
 declare_id!("98nD8KDJH5Vkov3a65xuPztmmk1eQHpdgR3Supk4RXb4");
 
 #[program]
@@ -7,7 +7,10 @@ pub mod gym_dapp_be {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Initializing the Gym Dapp");
+        let program_id = ctx.program_id;
+        let program_id_str = program_id.to_string();
+        msg!("Program ID: {}", program_id_str);
         Ok(())
     }
 }
